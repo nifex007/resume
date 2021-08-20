d935332ca69070c32c8c03eb0ae17f41c90e5bba
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-08-20 04:55:40 +0100

    access port from env

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get('*',function(req,res){
    });
 
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
